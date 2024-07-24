0868be2e16a54d3f272d14fe9498017bcb3a6de3
jadon-paulse <jadonpaulse@gmail.com>
2024-07-24 12:20:34 +0200

    Finished getURLsFromHTML function

--- crawl.js
+++ crawl.js
@@ -1,7 +1,5 @@
 import { JSDOM } from 'jsdom'
 
-
-
 // URL normalization still a work in progress
 function normalizeURL(url) {
     //const baseURL = 'blog.boot.dev/path'
@@ -33,7 +31,29 @@ function normalizeURL(url) {
 }
 
 function getURLsFromHTML(htmlBody, baseURL) {
-    
+    // const data = Array.prototype.slice.call(htmlBody).map(anchorTag => {
+    //     return anchorTag.textContent;
+    // })
+    // htmlBody.forEach(function (value) {
+    //     console.log(value.href)
+    // })
+    const urls = []
+    const dom = new JSDOM(htmlBody)
+    const anchors = dom.window.document.querySelectorAll('a')
+
+    for (const anchor of anchors) {
+        if (anchors.hasAttribute('href')) {
+            let href = anchor.getAttribute('href')
+
+            try {
+                href = new URL(href, baseURL).href
+                urls.push(href)
+            } catch (err) {
+                console.log(`${err.message}: ${href}`)
+            }
+        }
+    }
+    return urls
 }
 
-export { normalizeURL };
+export { normalizeURL, getURLsFromHTML };
--- crawl.test.js
+++ crawl.test.js
@@ -1,10 +1,28 @@
 
 import { test, expect } from "@jest/globals";
 import { normalizeURL } from "./crawl.js";
+import { getURLsFromHTML } from "./crawl.js";
+import { JSDOM } from 'jsdom'
 
+const dom = new JSDOM(`<!DOCTYPE html><body>
+    
+    <a href='https://blog.boot.dev/path'>Boot</a>
+    <a href='https://blog.boot.dev/path/'>Path</a>
+    <a href='https://google.com'>Path</a>
+    <a href='https://youtube.com'>Path</a>
+  
+  </body>`);
+console.log(dom.window.document.querySelector('a').textContent);
+// console.log(dom.window.document.querySelector('a').textContent);
 //const crawl = require('./crawl');
 const bootURL = 'https://blog.boot.dev/path'
+const baseURL = 'https://blog.boot.dev/path'
 
-test('check is url has a path called blog.boot.dev/path', () => {
-  normalizeURL(bootURL);
+// test('check if url has a path called blog.boot.dev/path', () => {
+//   normalizeURL(bootURL);
+// });
+
+test('check for html anchor tags and baseURL', () => {
+  console.log("Printed absolute url")
+  console.log(getURLsFromHTML(dom, baseURL));
 });
